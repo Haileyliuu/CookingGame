7946ecd7ceee60c5890b97d83dce825a740b92c2
isabelleLi-Projects <iggiesports10@gmail.com>
2025-10-09 21:53:04 -0700

    good arrow changes

--- cooking-game/Scripts/arrow.gd
+++ cooking-game/Scripts/arrow.gd
@@ -1,9 +1,12 @@
+class_name Arrow
 extends Node2D  # or RigidBody2D if you want physics
 
 @onready var tween = get_tree().create_tween()
 @export var speed: float = 1000  # launch speed
 #@export var target: Node2D  # assign your target object in the Inspector
 @onready var icon: Sprite2D = $Icon
+@onready var marker_2d: Marker2D = $"../Marker2D"
+
 
 
 var launched := false
@@ -13,8 +16,8 @@ func _ready():
 	swing()
 
 func swing():
-	tween.tween_property(self, "rotation_degrees", 120, 1.0)
-	tween.tween_property(self, "rotation_degrees", 240, 1.0)
+	tween.tween_property(self, "rotation_degrees", 120, 1)
+	tween.tween_property(self, "rotation_degrees", 240, 1)
 	tween.set_loops()
 
 func _process(delta):
@@ -22,7 +25,6 @@ func _process(delta):
 		position += velocity * delta
 		
 		
-#
 func launch():
 	if launched:
 		return
@@ -31,3 +33,32 @@ func launch():
 	var dir = Vector2.UP.rotated(rotation)  # change to RIGHT if arrow points right
 	velocity = dir.normalized() * speed
 	
+#hurtbox = the thing that does the damage
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.owner == null:
+		print("owner is null")
+		return
+	if area.get_parent().has_method("_animal"):
+		print("animal collision")
+		queue_free()
+		
+# flew off-screen
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	reset()
+
+# reset back to spawn point and swing again
+func reset():
+	launched = false
+	velocity = Vector2.ZERO
+	global_position = marker_2d.global_position
+	rotation = marker_2d.global_rotation
+
+	# Restart swing
+	tween = create_tween()
+	swing()
+
+
+	
+func _arrow():
+	pass
--- cooking-game/Scripts/minigame_hunting.gd
+++ cooking-game/Scripts/minigame_hunting.gd
@@ -1,8 +1,31 @@
 extends Node2D
-@onready var arrow: Sprite2D = $Arrow
+class_name MinigameHunting
+@onready var arrow: Node2D = $arrow
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
 		print("shoot pressed!")
 		arrow.launch()
 		
+
+##@export var arrow_scene: PackedScene = preload("res://Scenes/arrow.tscn")
+#@onready var marker_2d: Marker2D = $Marker2D
+#
+#var can_shoot = true
+#
+#func _process(delta):
+	#if Input.is_action_just_pressed("shoot") and can_shoot:
+		#shoot_arrow()
+#
+#func shoot_arrow():
+	#can_shoot = false
+	#arrow.launch()
+	##var arrow = load(arrow)
+	#get_tree().current_scene.add_child(arrow)  # or add_child if you want local parenting
+	#arrow.global_position = marker_2d.global_position
+	#arrow.rotation = marker_2d.global_rotation
+	#
+#
+	## simple reload timer
+	#await get_tree().create_timer(0.5).timeout
+	#can_shoot = true
