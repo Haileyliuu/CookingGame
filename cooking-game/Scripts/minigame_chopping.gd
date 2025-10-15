04f115dd6503d2bff0784822784ce99614ad15ee
Gianna <gpnicomedes@gmail.com>
2025-10-15 15:56:12 -0700

    New script

--- cooking-game/Scripts/minigame_chopping.gd
+++ cooking-game/Scripts/minigame_chopping.gd
@@ -1,8 +1,48 @@
 extends Node2D
 
-# Check if chop input is from cat side (left) or dog side (right)
+signal chop_done(player)
+
+const CHOPS_REQUIRED := 5
+
+var active_player : String = ""
+var chops_left : int = 0
+var chopping_active : bool = false
+
+@onready var chop_label = $ChopLabel
+@onready var prompt_label = $PromptLabel
+
+# TODO: Change locations of label nodes to be visible on the screen
+# i think that's the issue here
+
+
+func start_chopping(player: String):
+	active_player = player
+	chops_left = CHOPS_REQUIRED
+	chopping_active = true
+	prompt_label.text = "%s: Chop the food!" % player.capitalize()
+	update_label()
+
+
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("dog_chop"):
-		print("dog chop")
-	elif event.is_action_pressed("cat_chop"):
-		print("cat chop")
+	if not chopping_active:
+		return
+
+	if active_player == "dog" and event.is_action_pressed("dog_chop"):
+		handle_chop()
+
+	elif active_player == "cat" and event.is_action_pressed("cat_chop"):
+		handle_chop()
+
+
+func handle_chop():
+	chops_left -= 1
+	update_label()
+
+	if chops_left <= 0:
+		chopping_active = false
+		prompt_label.text = "%s finished chopping!" % active_player.capitalize()
+		emit_signal("chop_done", active_player)
+
+
+func update_label():
+	chop_label.text = "Chops left: %d" % chops_left
