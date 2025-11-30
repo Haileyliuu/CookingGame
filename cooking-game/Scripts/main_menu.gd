998b5d85d96a506ea50d0f8b85a3ba522a8ea3dd
Haileyliuu <haileyliuuu@gmail.com>
2025-11-29 22:26:19 -0800

    updated sprite animations and main menu scene added

---
+++ cooking-game/Scripts/main_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var play: TextureButton = $Play
+#@onready var credits: TextureButton = $Credits
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play.pressed.connect(_play_pressed)
+	#credits.pressed.connect(_credits_pressed)
+	#AudioPlayer.play_music_ui()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _play_pressed():
+	#AudioPlayer._play_button()
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _credits_pressed():
+	#AudioPlayer._play_button()
+	get_tree().change_scene_to_file("res://Scenes/UI elements/credits_screen.tscn")
+
+func _on_button_mouse_entered() -> void:
+	#AudioPlayer._play_hover()
+	pass
