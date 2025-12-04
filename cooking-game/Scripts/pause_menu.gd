acc488c2a05adf61561fe8cbe2ebb15b1ebe81b9
Haileyliuu <haileyliuuu@gmail.com>
2025-12-04 15:07:04 -0800

    edited pause menu

--- cooking-game/Scripts/pause_menu.gd
+++ cooking-game/Scripts/pause_menu.gd
@@ -42,10 +42,6 @@ func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
 
 
-func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
-
-
 func _on_pause_button_pressed() -> void:
 	if get_tree().paused:
 		resume()
