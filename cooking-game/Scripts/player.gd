960c6e4a3cc7d6bfd40802d0eed7b44e084b6792
isabelleLi-Projects <iggiesports10@gmail.com>
2025-12-04 10:49:13 -0800

    added pause_button

--- cooking-game/Scripts/player.gd
+++ cooking-game/Scripts/player.gd
@@ -29,6 +29,10 @@ func _process(_delta: float) -> void:
 
 
 func get_input():
+	if get_tree().paused:
+		velocity = Vector2.ZERO
+		return
+	
 	var input_direction = Input.get_vector(player_id + "_left", player_id + "_right", player_id + "_up", player_id + "_down")
 	velocity = input_direction * speed
 	
