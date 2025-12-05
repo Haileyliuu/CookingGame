6774e243a855dbd3baedb74d6b0efb449f9b0ad6
isabelleLi-Projects <iggiesports10@gmail.com>
2025-12-04 18:21:58 -0800

    added back blur

--- cooking-game/Scripts/inventory_fish.gd
+++ cooking-game/Scripts/inventory_fish.gd
@@ -4,3 +4,7 @@ extends Label
 
 func _process(_delta: float) -> void:
 	label.text = ":" + str(Inventory.cat_hunted_meat) + "\n:" + str(Inventory.cat_chopped_meat)
+
+
+func _on_pause_button_pressed() -> void:
+	pass # Replace with function body.
