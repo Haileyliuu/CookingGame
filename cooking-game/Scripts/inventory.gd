2444edd87681d3286405fedc508099479908f0b5
Wayfleh <rafael.caculba04@gmail.com>
2025-12-04 17:50:21 -0800

    maybe buggy

--- cooking-game/Scripts/inventory.gd
+++ cooking-game/Scripts/inventory.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var cat_hunted_meat = 100
-var cat_chopped_meat = 100
-var dog_hunted_meat = 1
+var cat_hunted_meat = 0
+var cat_chopped_meat = 0
+var dog_hunted_meat = 0
 var dog_chopped_meat = 0
